df46da1dec4ac69366bdf64e77a3ef6295372328
rupsd124 <rupsd124@gmail.com>
2024-04-16 18:35:44 +0530

    merge conflict resolve 1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-        return 'Hello, world! This is a Flask web application, hello here.'
+        return 'Hello, world! This is a Flask web application, hi jenkins.'
 
 if __name__ == '__main__':
         app.run(debug=False, host='0.0.0.0', port=5000)
